3122e7fb1308246d7395b688bd4b2f6451a07043
Roman Serediuk <box4rom@gmail.com>
2023-04-19 10:35:23 +0300

    fix: add status: :unprocessable_entity to update/create actions

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -18,7 +18,7 @@ class QuestionsController < ApplicationController
     if @question.update question_params
       redirect_to questions_path
     else
-      render :edit
+      render :edit, status: :unprocessable_entity
     end
   end
 
@@ -35,7 +35,7 @@ class QuestionsController < ApplicationController
     if @question.save
       redirect_to questions_path
     else
-      render :new
+      render :new, status: :unprocessable_entity
     end
   end
 
