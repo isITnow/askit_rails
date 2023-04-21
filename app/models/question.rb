56936d3e933974f0c70d4e8966c3f95f10ee8f28
Roman Serediuk <box4rom@gmail.com>
2023-04-21 13:47:31 +0300

    feat: default_scope oder by updated_at

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,5 @@
 class Question < ApplicationRecord
+  default_scope {order(updated_at: :desc)}
   has_many :answers, dependent: :destroy
   validates :title, presence: true, length: {minimum: 2}
   validates :body, presence: true, length: {minimum: 2}
