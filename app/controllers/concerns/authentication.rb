30015d6bea6cc242335fe0d961b21c70615f4fff
Roman Serediuk <box4rom@gmail.com>
2023-04-24 19:29:55 +0300

    feat: create equire_authentication, require_no_authentication methods

--- app/controllers/concerns/authentication.rb
+++ app/controllers/concerns/authentication.rb
@@ -20,6 +20,21 @@ module Authentication
         @current_user = nil
       end
       
+      def require_no_authentication
+        return unless user_signed_in?
+        
+        flash[:warning] = "You are already signed in"
+        redirect_to root_path
+      end
+
+      def require_authentication
+        return if user_signed_in?
+
+        flash[:warning] = "You are not signed in"
+        redirect_to root_path
+      end
+      
+      
       
       helper_method :current_user, :user_signed_in?
   end
