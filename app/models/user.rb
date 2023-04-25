02a943eb2267d85d5d62f9e5ddeed3e6fb6748ba
Roman Serediuk <box4rom@gmail.com>
2023-04-25 12:00:33 +0300

    feat: add email validation with valid_email_2 gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
   
-  validates :email, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true, 'valid_email_2/email': true
   validates :name, presence: true
 end
