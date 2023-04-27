e242a1f0211e7f9d8e9353441ba0ec6f4dff3161
Roman Serediuk <box4rom@gmail.com>
2023-04-27 21:31:53 +0300

    feat: create locale scope

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,19 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resource :session, only: %i[new create destroy]
-
-  resources :users, except: %i[index show destroy]
-
-  resources :questions do
-    resources :answers, except: %i[show new]
+  scope '(:locale)', locale: /#{I18n.available_locales.join('|')}/ do
+    resource :session, only: %i[new create destroy]
+    
+    resources :users, except: %i[index show destroy]
+    
+    resources :questions do
+      resources :answers, except: %i[show new]
+    end
+    
+    namespace :admin do
+      resources :users, only: %i[index]
+    end
+    
+    root 'pages#index'
   end
-
-  namespace :admin do
-    resources :users, only: %i[index]
-  end
-
-  root 'pages#index'
 end
