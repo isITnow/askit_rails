1ef5074537ef2be8d0a80d3588210ab59e9c0e05
Roman Serediuk <box4rom@gmail.com>
2023-04-27 16:03:52 +0300

    feat: implement i18n

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -10,7 +10,7 @@ class QuestionsController < ApplicationController
 
   def destroy
     @question.destroy
-    flash[:success] = 'Question deleted!'
+    flash[:success] = t('.success')
     redirect_to questions_path
   end
 
@@ -18,7 +18,7 @@ class QuestionsController < ApplicationController
 
   def update
     if @question.update question_params
-      flash[:success] = 'Question updated!'
+      flash[:success] = t('.success')
       redirect_to questions_path
     else
       render :edit, status: :unprocessable_entity
@@ -36,7 +36,7 @@ class QuestionsController < ApplicationController
   def create
     @question = Question.new question_params
     if @question.save
-      flash[:success] = 'Question created!'
+      flash[:success] = t('.success')
       redirect_to questions_path
     else
       render :new, status: :unprocessable_entity
