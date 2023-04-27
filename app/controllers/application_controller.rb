3e4b2c62ae0e48f22d1d2cb760aa3b65f41336ce
Roman Serediuk <box4rom@gmail.com>
2023-04-27 21:36:10 +0300

    feat: add around_action :switch_locale

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,22 @@ class ApplicationController < ActionController::Base
   include Pagy::Backend
   include ErrorHandling
   include Authentication
+
+  around_action :switch_locale
+
+  def switch_locale(&action)
+    locale = locale_from_url || I18n.default_locale
+    I18n.with_locale locale, &action
+  end
+
+  def locale_from_url
+    locale = params[:locale]
+    I18n.available_locales.map(&:to_s).include?(locale) ? locale : nil
+  end
+  
+  def default_url_options
+    { locale: I18n.locale }
+  end
+  
+  
 end
