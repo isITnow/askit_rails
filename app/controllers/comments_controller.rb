7382c7ad69fb8cb1db639c38121e9d02b65e89cd
Roman Serediuk <box4rom@gmail.com>
2023-05-01 17:45:07 +0300

    feat: create CommentsController

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+class CommentsController < ApplicationController
+  # include QuestionsAnswers
+
+  before_action :set_commentable!
+  before_action :set_question
+
+  def create
+    @comment = @commentable.comments.build comment_params
+    if @comment.save 
+      flash[:success] = t('.success')
+      redirect_to question_path(@question)
+    else
+      # load_question_answers do_render: true
+      @pagy, @answers = pagy(@question.answers.includes(:user))
+      render 'questions/show', status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    comment = @commentable.comments.find params[:id]
+    comment.destroy
+    flash[:success] = t '.success'
+    redirect_to question_path(@question)
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:body).merge(user_id: current_user.id)
+  end
+
+  def set_commentable!
+    klass = [Question, Answer].detect { |c| params["#{c.name.underscore}_id"] }
+    raise ActiveRecord::RecordNotFound if klass.blank?
+
+    @commentable = klass.find(params["#{klass.name.underscore}_id"])
+  end
+  
+  def set_question
+    @question = @commentable.is_a?(Question) ? @commentable : @commentable.question
+  end
+  
+end
