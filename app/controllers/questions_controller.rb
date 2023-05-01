24e41f3cb01d46c756f83289782fceaff6002b04
Roman Serediuk <box4rom@gmail.com>
2023-05-01 19:44:08 +0300

    fix: index act - request with scope; add fetch_tags before_action

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,6 +3,7 @@
 class QuestionsController < ApplicationController
   # include QuestionsAnswers
   before_action :set_question!, only: %i[show destroy edit update]
+  before_action :fetch_tags, only: %i[new edit]
 
   def show
     # load_question_answers
@@ -28,7 +29,9 @@ class QuestionsController < ApplicationController
   end
 
   def index
-    @pagy, @questions = pagy(Question.includes(:user))
+    @pagy, @questions = pagy Question.all_by_tags(params[:tag_ids])
+    # @tags = Tag.where(id: params[:tag_ids]) if params[:tag_ids]
+    # @pagy, @questions = pagy Question.all_by_tags(@tags)
   end
 
   def new
@@ -52,10 +55,15 @@ class QuestionsController < ApplicationController
   private
 
   def question_params
-    params.require(:question).permit(:title, :body)
+    params.require(:question).permit(:title, :body, tag_ids: [])
   end
 
   def set_question!
     @question = Question.find params[:id]
   end
+
+  def fetch_tags
+    @tags = Tag.all
+  end
+  
 end
