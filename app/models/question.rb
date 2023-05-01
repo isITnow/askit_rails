4491bf5a1ff304c02248d9858837ac80fd31448a
Roman Serediuk <box4rom@gmail.com>
2023-05-01 19:42:07 +0300

    feat: create scope :all_by_tags

--- app/models/question.rb
+++ app/models/question.rb
@@ -3,8 +3,6 @@
 class Question < ApplicationRecord
   include Commentable
   
-  default_scope { order(updated_at: :desc) }
-
   has_many :answers, dependent: :destroy
   belongs_to :user
   has_many :question_tags, dependent: :destroy
@@ -20,4 +18,23 @@ class Question < ApplicationRecord
   def formatted_edited_at
     updated_at.strftime('%Y-%m-%d %H:%M:%S')
   end
+
+  # scope :all_by_tags, -> (tag_ids) do
+  #   questions = includes(:user, :question_tags, :tags)
+  #   questions = questions.joins(:tags).where(tags: tag_ids) if tag_ids
+  #   questions.order(updated_at: :desc)
+  # end
+
+  scope :all_by_tags, ->(tags) do
+    questions = includes(:user)
+    
+    if tags
+      questions = questions.joins(:tags).where(tags: tags).preload(:tags)
+    else
+      questions = questions.includes(:question_tags, :tags)
+    end
+    
+    questions.order(updated_at: :desc)
+  end
+
 end
