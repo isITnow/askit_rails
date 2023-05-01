e33aa3f51bf9dbbc2f421874c460f7139371392c
Roman Serediuk <box4rom@gmail.com>
2023-05-01 11:46:17 +0300

    feat: implement dependency with comments

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,7 +1,10 @@
 # frozen_string_literal: true
 
 class Answer < ApplicationRecord
+  include Commentable
+
   default_scope { order(updated_at: :desc) }
+  
   belongs_to :question
   belongs_to :user
 
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,9 +1,13 @@
 # frozen_string_literal: true
 
 class Question < ApplicationRecord
+  include Commentable
+  
   default_scope { order(updated_at: :desc) }
+
   has_many :answers, dependent: :destroy
   belongs_to :user
+
   validates :title, presence: true, length: { minimum: 2 }
   validates :body, presence: true, length: { minimum: 2 }
 
