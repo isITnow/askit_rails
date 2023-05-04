7466efb9689c2cd532e39e1a5b38793c54bd4776
Roman Serediuk <box4rom@gmail.com>
2023-05-04 13:31:09 +0300

    fix: error message

--- app/validators/email_validator.rb
+++ app/validators/email_validator.rb
@@ -1,6 +1,6 @@
 class EmailValidator < ActiveModel::EachValidator
   def validate_each(rec, att, val)
-    rec.errors.add(att, (options[:message] || "Invalid format")) unless valid_email?(val)
+    rec.errors.add(att, (options[:message] || "has invalid format")) unless valid_email?(val)
   end
 
   private
