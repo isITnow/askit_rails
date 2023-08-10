ec0d31ed73c9517d6d51db934f4868b64f09f173
Roman Serediuk <box4rom@gmail.com>
2023-08-10 13:02:10 +0300

    upd: implement FactoryBot; add context "when creating a question"

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,41 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Question, type: :model do
-  let(:user) { User.new name: 'John Doe', email: 'jdoe1@example.com',
-                                        password: 'password', password_confirmation: 'password' }
-  let(:question) { Question.new title: 'test Title', body: 'test body' }
-
-  it 'return question title' do
-    question.user = user
-    expect(question.title).to eq('test Title')
-  end
-
-  it 'valid with valid attributes' do
-    question.user = user
-    expect(question).to be_valid
-  end
-
-  it 'is not valid without title' do
-    question.user = user
-    question.title = nil
-    expect(question).to_not be_valid
-  end
-
-  it 'is not valid with a title less than 2 characters long' do
-    question.title = '1'
-    expect(question).to_not be_valid
-  end
-
-  it 'is not valid without body' do
-    question.user = user
-    question.body = nil
-    expect(question).to_not be_valid
-  end
-
-  it 'is not valid with a body less than 2 characters long' do
-    question.body = '1'
-    expect(question).to_not be_valid
-  end
+  subject { create :question }
 
   describe 'associations' do
     it { should have_many(:answers).class_name('Answer').dependent(:destroy) }
@@ -51,5 +17,31 @@ RSpec.describe Question, type: :model do
 
     it { should validate_length_of(:title).is_at_least(2) }
     it { should validate_length_of(:body).is_at_least(2) }
+  end
+
+  context "when creating a question" do
+    it 'valid with valid attributes' do
+      expect(subject).to be_valid
+    end
+
+      it 'is not valid without title' do
+      subject.title = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'is not valid without body' do
+      subject.body = nil
+      expect(subject).to_not be_valid
+    end
+  end
+
+  it 'is not valid with a title less than 2 characters long' do
+    subject.title = '1'
+    expect(subject).to_not be_valid
+  end
+
+  it 'is not valid with a body less than 2 characters long' do
+    subject.body = '1'
+    expect(subject).to_not be_valid
   end
 end
