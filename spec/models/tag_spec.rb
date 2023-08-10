5f0e9d56701ca6cea5bdc61f0ad79d25b4a12601
Roman Serediuk <box4rom@gmail.com>
2023-08-10 10:01:59 +0300

    feat: add tag validations and relations tests

---
+++ spec/models/tag_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe Tag, type: :model do
+  let(:tag) { Tag.new title: "foo" }
+  describe 'validations' do
+    it { should validate_presence_of(:title) }
+    it { should validate_uniqueness_of(:title) }
+  end
+
+  describe 'associations' do
+    it { should have_many(:question_tags).dependent(:destroy)}
+    it { should have_many(:questions).class_name('Question').through(:question_tags) }
+  end
+end
